10da1b2add1c6be0b601096a4a5483229e4fddc6
Wisdom Eli <104230238+Grandlhord@users.noreply.github.com>
2023-05-20 07:59:18 +0000

    Addition of other functionalities

--- script.js
+++ script.js
@@ -1,108 +1,134 @@
-let form=document.querySelector("form");
-let text=document.getElementById("text");
-let todoCon=document.querySelector(".todo-con")
-form.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    addtodo();
-})
-let todos=JSON.parse(localStorage.getItem("todos"));
-if(todos){
-    todos.forEach(element => {
-        addtodo(element)
-    });
-}
-function addtodo(elem) {
-    let todoColl=document.createElement("div");
-    todoColl.classList.add("todocoll")
-    let todotext=text.value;
-    if(elem){
-        todotext=elem.text;
-    }
-    if(todotext){
-    todoColl.innerHTML=`
-    <div class="todo-li">
-    <div class="check ${elem && elem.complete?"active-check":""}"><img src="icon-check.svg" alt=""></div>
-    <p class="ptag ${elem && elem.complete?"complete":""}">${todotext}</p>
-    <button class="close"><img src="icon-cross.svg" alt=""></button>
-  </div>
-  <div class="hr"></div>`;
-    todoCon.appendChild(todoColl);
-    updateLs()
-    }
-    let close=todoColl.querySelector(".close");
-    close.addEventListener("click", ()=>{
-        todoColl.remove();
-        updateLs();
-    })
-    let check=todoColl.querySelector(".check");
-    check.addEventListener('click', ()=>{
-        check.classList.toggle("active-check")
-        todoColl.children[0].children[1].classList.add("complete")  
-        updateLs()
-    })
-    text.value="";
-}
+let form = document.querySelector("form");
+let text = document.getElementById("text");
+let todoCon = document.querySelector(".todo-con");
+let left = document.querySelector(".left");
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addTodo();
+});
 
 function updateLs() {
-    let ptag=document.querySelectorAll(".ptag")
-    let arr=[];
-    ptag.forEach(element => {
-        arr.push({
-            text:element.innerText,
-            complete:element.classList.contains("complete")
-        })
-    });
-    localStorage.setItem("todos",JSON.stringify(arr));
+  localStorage.setItem("todos", JSON.stringify(todos));
+  updateItemsLeft();
 }
-let info=document.querySelectorAll(".choice p")
-let todoli=document.querySelectorAll(".todocoll")
-console.log(info);
-info.forEach(element => {
-   element.addEventListener("click", ()=>{
-    info.forEach(item => {
-        item.classList.remove("active");
-    });
-    element.classList.add("active")
-    if(element.innerText=="Active"){
-        todoli.forEach(elem => {
-            if(!elem.children[0].children[1].classList.contains("complete")){
-                elem.style.display="block";
-            }else{
-                elem.style.display="none";
-            }
-        });
-    }else if(element.innerText=="Completed"){
-        todoli.forEach(elem => {
-            if(elem.children[0].children[1].classList.contains("complete")){
-                elem.style.display="block";
-            }else{
-                elem.style.display="none";
-            }
-        });
-    }else{
-        todoli.forEach(elem => {
-            elem.style.display="block";
-        });
+
+function addTodo() {
+  let todotext = text.value;
+  if (todotext) {
+    let newTodo = {
+      text: todotext,
+      complete: false
+    };
+    todos.push(newTodo);
+    updateLs();
+    renderTodoItem(newTodo);
+    text.value = "";
+  }
+}
+
+function renderTodoItem(todo) {
+  let todoColl = document.createElement("div");
+  todoColl.classList.add("todocoll");
+  todoColl.draggable = true;
+
+  let completeClass = todo.complete ? "complete" : "";
+  todoColl.innerHTML = `
+    <div class="todo-li">
+      <div class="check ${todo.complete ? "active-check" : ""}"><img src="icon-check.svg" alt=""></div>
+      <p class="ptag ${completeClass}">${todo.text}</p>
+      <button class="close"><img src="icon-cross.svg" alt=""></button>
+    </div>
+    <div class="hr"></div>`;
+
+  todoCon.appendChild(todoColl);
+
+  let close = todoColl.querySelector(".close");
+  close.addEventListener("click", () => {
+    todoColl.remove();
+    todos = todos.filter(item => item !== todo);
+    updateLs();
+    updateItemsLeft();
+  });
+
+  let check = todoColl.querySelector(".check");
+  check.addEventListener('click', () => {
+    todo.complete = !todo.complete;
+    check.classList.toggle("active-check");
+    todoColl.children[0].children[1].classList.toggle("complete");
+    updateLs();
+    updateItemsLeft();
+  });
+
+  updateItemsLeft();
+
+  todoColl.addEventListener("dragstart", handleDragStart);
+  todoColl.addEventListener("dragover", handleDragOver);
+  todoColl.addEventListener("drop", handleDrop);
+}
+
+function updateItemsLeft() {
+  let activeTodos = todos.filter(todo => !todo.complete);
+  let count = activeTodos.length;
+  left.innerText = `${count} item${count === 1 ? "" : "s"} left`;
+}
+
+function filterTodos(filter) {
+  let todoItems = todoCon.getElementsByClassName("todocoll");
+  Array.from(todoItems).forEach(todoItem => {
+    let isCompleted = todoItem.getElementsByClassName("ptag")[0].classList.contains("complete");
+    if (filter === "active") {
+      todoItem.style.display = "block";
+    } else if (filter === "Active") {
+      todoItem.style.display = isCompleted ? "none" : "block";
+    } else if (filter === "Completed") {
+      todoItem.style.display = isCompleted ? "block" : "none";
     }
-   })
-});
-let clear=document.querySelector(".clear");
-clear.addEventListener("click", ()=>{
-    todoli.forEach(elem => {
-        if(elem.children[0].children[1].classList.contains("complete")){
-            elem.remove()
-            updateLs();
-        }
-    });
-})
-let left=document.querySelector(".left");
-function setitem() {
-    let activeTodo=document.querySelectorAll(".todo-li .active-check");
-    let diff=todoli.length-activeTodo.length;
-    left.innerText=`${diff} items left`
-    
+  });
 }
-setitem();
+
+let filterButtons = document.querySelectorAll(".option p");
+filterButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    filterButtons.forEach(btn => btn.classList.remove("active"));
+    button.classList.add("active");
+    let filter = button.classList[0];
+    filterTodos(filter);
+  });
+});
+
+let clear = document.querySelector(".clear");
+clear.addEventListener("click", () => {
+  todos = todos.filter(todo => !todo.complete);
+  let completedItems = todoCon.getElementsByClassName("complete");
+  Array.from(completedItems).forEach(item => item.parentElement.parentElement.remove());
+  updateLs();
+  updateItemsLeft();
+});
+
+let draggedItem = null;
+
+function handleDragStart() {
+  draggedItem = this;
+}
+
+function handleDragOver(event) {
+  event.preventDefault();
+}
+
+function handleDrop() {
+  const droppedItem = this;
+  const droppedIndex = Array.from(todoCon.children).indexOf(droppedItem);
+  const draggedIndex = Array.from(todoCon.children).indexOf(draggedItem);
+  const temp = todos[droppedIndex];
+  todos[droppedIndex] = todos[draggedIndex];
+  todos[draggedIndex] = temp;
+  todoCon.insertBefore(draggedItem, droppedItem);
+  updateLs();
+}
+
+todos.forEach(todo => renderTodoItem(todo));
 
 
 
